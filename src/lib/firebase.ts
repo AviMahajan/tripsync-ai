73a791c449751eb8757751cf1dd6b2f427544bc2
AviMahajan <40890634+AviMahajan@users.noreply.github.com>
2026-09-19 17:08:14 +0530

    Update firebase.ts
    adding default database

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app, config.firestoreDatabaseId);
+const db = getFirestore(app);
 
 export const firebaseService = {
   db,
